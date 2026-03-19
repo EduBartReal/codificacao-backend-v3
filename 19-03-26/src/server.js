4aece1871fbb54ac5712e1740b984cd6b437b7d5
EduBartReal <edubartellt@gmail.com>
2026-03-19 15:58:16 -0300

    envio da atv1 19/03

---
+++ 19-03-26/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+const port = 3000
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json('hi')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`)
+})
